6169634dd07df1fe92ac8b641ca0f150b640cb00
Nikitonu4 <nikitonu4@gmail.com>
2020-05-17 19:08:41 +0300

    Serve static files only in dev mode

--- lab3/book_app.rb
+++ lab3/book_app.rb
@@ -4,7 +4,16 @@ require 'roda'
 
 # BookApp class of application
 class BookApp < Roda
+  opts[:root] = __dir__
+  plugin :environments
+
+  configure :development do
+    plugin :public
+    opts[:serve_static] = true
+  end
+
   route do |r|
+    r.public if opts[:serve_static]
     r.root do
       'Hello, world!'
     end
