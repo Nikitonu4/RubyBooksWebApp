66232821425178b9b782a9f62304f2930832851b
Nikitonu4 <nikitonu4@gmail.com>
2020-06-19 18:34:19 +0300

    delete files

--- lab5 2/models/books/book.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-# The information about the book
-Book = Struct.new(:id, :name, :author, :genre, :price, :quantity, keyword_init: true)
--- lab5 2/models/lists/product_add_form_schema.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'dry-schema'
-
-require_relative '../schema_types'
-
-ProductAddFormSchema = Dry::Schema.Params do
-  required(:name).filled(SchemaTypes::StrippedString)
-end
